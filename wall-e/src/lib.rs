d94b6ac7946c76d5e6e91b0429c6633dcf293ec7
Devin Leamy <devinleamy@gmail.com>
2023-11-01 19:24:56 -0400

    Made it into a workspace so I could put all the scripting stuff into a different crate

--- wall-e/src/lib.rs
+++ wall-e/src/lib.rs
@@ -1,12 +1,8 @@
 mod output;
 mod scene;
 
-mod prelude {
+pub mod prelude {
     pub use crate::output::*;
     pub use crate::scene::*;
     pub use nalgebra::Vector3;
 }
-
-use prelude::*;
-
-fn main() {}
