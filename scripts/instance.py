6073bac6849a8ebd75eee87f77cb6e81dbab371b
Devin Leamy <devinleamy@gmail.com>
2023-11-08 14:54:04 -0500

    Cleaned up instance script

--- scripts/instance.py
+++ scripts/instance.py
@@ -11,7 +11,6 @@ n_scene.rotate('X', 23)
 plane = Mesh('plane.obj')
 plane.set_material(grass)
 plane.scale(30, 30, 30)
-# plane.scale(30, 30, 30)
 n_scene.add_child(plane)
 
 sphere = Geometry('sphere')
@@ -19,28 +18,28 @@ sphere.scale(2.5, 2.5, 2.5)
 sphere.set_material(stone)
 n_scene.add_child(sphere)
 
+arc = Transform()
+arc.translate(0, 0, -10)
+
+p1 = Geometry("cube")
+p1.set_material(stone)
+p1.scale(0.8, 4, 0.8)
+p1.translate(-2.4, 0, -0.4)
+arc.add_child(p1)
+
+p2 = Geometry("cube")
+p2.set_material(stone)
+p2.scale(0.8, 4, 0.8)
+p2.translate(1.6, 0, -0.4)
+arc.add_child(p2)
+
+s = Geometry("sphere")
+s.set_material(stone)
+s.scale(4, 0.6, 0.6)
+s.translate(0, 4, 0)
+arc.add_child(s)
+
 for i in range(6):
-    arc = Transform()
-    arc.translate(0, 0, -10)
-
-    p1 = Geometry("cube")
-    p1.set_material(stone)
-    p1.scale(0.8, 4, 0.8)
-    p1.translate(-2.4, 0, -0.4)
-    arc.add_child(p1)
-
-    p2 = Geometry("cube")
-    p2.set_material(stone)
-    p2.scale(0.8, 4, 0.8)
-    p2.translate(1.6, 0, -0.4)
-    arc.add_child(p2)
-
-    s = Geometry("sphere")
-    s.set_material(stone)
-    s.scale(4, 0.6, 0.6)
-    s.translate(0, 4, 0)
-    arc.add_child(s)
-
     n_arc = Transform()
     n_arc.rotate('Y', i * 60)
     n_arc.add_child(arc)
@@ -53,6 +52,5 @@ n_scene.add_child(light)
 camera = Camera((0, 2, 30), (0, 0, -1), (0, 1, 0), 50)
 scene.set_root(n_scene)
 
-copy_and_archive_image()
-
 ray_trace(scene, camera, 256, 256, "image.png")
+copy_and_archive_image()
